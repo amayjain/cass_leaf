e39357380de76ebae0c679db778a849d07073fbc
Amay Jain <amayjain@Amays-MacBook-Air.local>
2021-04-22 20:46:38 -0700

    pillow trial

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -1,5 +1,8 @@
 import torch
-
+import torchvision
+import pandas as pd
+from PIL import Image
+import matplotlib.pyplot as plt
 
 class StartingDataset(torch.utils.data.Dataset):
     """
@@ -7,13 +10,20 @@ class StartingDataset(torch.utils.data.Dataset):
     """
 
     def __init__(self):
-        pass
+        self.data = pd.read_csv('../cass_data/train.csv')
+
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        # PIL.Image.open(fp, mode='r', formats=None)
+        with Image.open("../cass_data/train_images/6103.jpg") as im:
+            im.rotate().show()
+        # inputs = torch.zeros([3, 224, 224])
+        # label = 0
 
         return inputs, label
 
     def __len__(self):
         return 10000
+
+
+x = StartingDataset()
