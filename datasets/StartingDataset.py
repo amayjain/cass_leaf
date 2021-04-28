b820edb86e25a1d347edc00f9b2591862edf3d51
Amay Jain <amayjain@Amays-MacBook-Air.local>
2021-04-27 20:35:37 -0700

    loaded data

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -9,8 +9,11 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self):
-        self.data = pd.read_csv('../cass_data/train.csv')
+    def __init__(self, train_check):
+        if train_check == true:
+            self.data = pd.read_csv('../cass_data/train.csv') # 80%
+        else:
+            self.data = pd.read_csv('../cass_data/train.csv') # 20%
 
 
     def __getitem__(self, index):
@@ -40,16 +43,11 @@ class StartingDataset(torch.utils.data.Dataset):
         return 21367
 
 
-x = StartingDataset()
+leaf_traindata = StartingDataset(true)
+leaf_testdata = StartingDataset(false)
 #print(x.__getitem__(23))
 #x.data.head()
 
+leaf_traindataload = torch.utils.data.DataLoader(leaf_traindata, batch_size= 16, shuffle=True)
+leaf_testdataload = torch.utils.data.DataLoader(leaf_testdata, batch_size= 16, shuffle=True)
 
-"""
-for i in range(26):
-    print(x.__getitem__(i))
-"""
-
-#finish __getitem__
-#finish len
-#finish info doc
